c7798e4d79c139e10760ea7cd917ebf077814b0f
weilycoder <weily2009@outlook.com>
2026-08-10 14:20:05 +0800

    feat: Enhance cnEn function to improve space insertion logic

--- src/formatter/space/cn-en.ts
+++ src/formatter/space/cn-en.ts
@@ -1,6 +1,7 @@
 import { visit } from "unist-util-visit";
-import type { Root } from "mdast";
-import { isCJK, isLatin } from "../utils";
+import type { Root, Literal } from "mdast";
+import { isCJK, isLatin, getLastChar } from "../utils";
+import { insertBeforeIf } from "./utils";
 
 function addCnEnSpace(line: string): string {
   let result = "";
@@ -21,4 +22,22 @@ export function cnEn(tree: Root) {
   visit(tree, "text", (node) => {
     node.value = addCnEnSpace(node.value);
   });
+  insertBeforeIf(
+    tree,
+    (node, index, parent) => {
+      if (!parent || index === undefined) return false;
+      const currValue = (node as Literal)?.value;
+      if (typeof currValue !== "string") return false;
+      if (currValue.length === 0) return false;
+      const currChar = currValue[0];
+      const prevNode = parent.children[index - 1];
+      const prevChar = getLastChar(prevNode);
+      if (prevChar === undefined) return false;
+      return (
+        (isCJK(prevChar) && isLatin(currChar)) ||
+        (isLatin(prevChar) && isCJK(currChar))
+      );
+    },
+    " ",
+  );
 }
