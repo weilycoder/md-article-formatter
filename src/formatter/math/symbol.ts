db7f22ae85ff87af1e1bd9db9174b03223b57664
weilycoder <weily2009@outlook.com>
2026-08-14 14:37:23 +0800

    fix: Use a cloned replace map for sorting in symbolReplace function

--- src/formatter/math/symbol.ts
+++ src/formatter/math/symbol.ts
@@ -5,11 +5,12 @@ import type { MapEntry } from "../formatter";
 
 export function symbolReplace(tree: Root, replaceTable: MapEntry[]) {
   if (replaceTable.length === 0) return;
-  replaceTable.sort((a, b) => b.from.length - a.from.length);
+  const replaceMap = structuredClone(replaceTable);
+  replaceMap.sort((a, b) => b.from.length - a.from.length);
   visit(tree, (node, index, parent) => {
     if (!parent || index === undefined) return;
     if (node.type !== "math" && node.type !== "inlineMath") return;
-    for (const { enabled, from, to } of replaceTable)
+    for (const { enabled, from, to } of replaceMap)
       if (enabled) node.value = node.value.replaceAll(from, " " + to + " ");
   });
 }
